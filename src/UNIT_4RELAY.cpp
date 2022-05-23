13c75b5dfe239f8f413f63918113751482d01486
Tinyu <Tinyu.Zhao@gmail.com>
2022-05-23 14:19:04 +0800

    allow define wire

--- src/UNIT_4RELAY.cpp
+++ src/UNIT_4RELAY.cpp
@@ -1,74 +1,64 @@
 #include "UNIT_4RELAY.h"
 
-
-
-void UNIT_4RELAY::write1Byte(uint8_t address, uint8_t Register_address, uint8_t data)
-{
-    Wire.beginTransmission(address);
-    Wire.write(Register_address);  
-    Wire.write(data);
-    Wire.endTransmission();
-
+void UNIT_4RELAY::Init(bool mode) {
+    write1Byte(DEVICE_I2C_ADDR, mode_Reg, mode);
+    write1Byte(DEVICE_I2C_ADDR, relay_Reg, 0);
 }
 
-
-uint8_t  UNIT_4RELAY::read1Byte(uint8_t address, uint8_t Register_address)
-{
-    Wire.beginTransmission(address);   // Initialize the Tx buffer
-    Wire.write(Register_address);            // Put slave register address in Tx buffer
-    Wire.endTransmission();
-    Wire.requestFrom(address, 1);
-    uint8_t data = Wire.read();
-    return data; 
+void UNIT_4RELAY::begin(TwoWire *wire, uint8_t sda, uint8_t scl,
+                        uint32_t freq) {
+    _wire = wire;
+    _sda  = sda;
+    _scl  = scl;
+    _freq = freq;
+    _wire->begin(DEVICE_I2C_ADDR, _sda, _scl, _freq);
 }
 
-
-
-void UNIT_4RELAY::relayALL(bool state)
-{
-    write1Byte(addr,relay_Reg,state*(0x0f));
+void UNIT_4RELAY::write1Byte(uint8_t address, uint8_t Register_address,
+                             uint8_t data) {
+    _wire->beginTransmission(address);
+    _wire->write(Register_address);
+    _wire->write(data);
+    _wire->endTransmission();
 }
 
-void UNIT_4RELAY::LED_ALL(bool state)
-{
-    write1Byte(addr,relay_Reg,state*(0xf0));
+uint8_t UNIT_4RELAY::read1Byte(uint8_t address, uint8_t Register_address) {
+    _wire->beginTransmission(address);  // Initialize the Tx buffer
+    _wire->write(Register_address);  // Put slave register address in Tx buffer
+    _wire->endTransmission();
+    _wire->requestFrom(address, uint8_t(1));
+    uint8_t data = _wire->read();
+    return data;
 }
 
-void UNIT_4RELAY::relayWrite( uint8_t number, bool state )
-{
-    uint8_t StateFromDevice = read1Byte(addr, relay_Reg);
-    if( state == 0 )
-    {
-        StateFromDevice &= ~( 0x01 << number );
+void UNIT_4RELAY::relayALL(bool state) {
+    write1Byte(DEVICE_I2C_ADDR, relay_Reg, state * (0x0f));
+}
+
+void UNIT_4RELAY::LED_ALL(bool state) {
+    write1Byte(DEVICE_I2C_ADDR, relay_Reg, state * (0xf0));
+}
+
+void UNIT_4RELAY::relayWrite(uint8_t number, bool state) {
+    uint8_t StateFromDevice = read1Byte(DEVICE_I2C_ADDR, relay_Reg);
+    if (state == 0) {
+        StateFromDevice &= ~(0x01 << number);
+    } else {
+        StateFromDevice |= (0x01 << number);
     }
-    else
-    {
-        StateFromDevice |= ( 0x01 << number );
-    }
-    write1Byte(addr,relay_Reg,StateFromDevice);
+    write1Byte(DEVICE_I2C_ADDR, relay_Reg, StateFromDevice);
 }
 
-void UNIT_4RELAY::LEDWrite( uint8_t number, bool state )
-{
-    uint8_t StateFromDevice = read1Byte(addr, relay_Reg);
-    if( state == 0 )
-    {
-        StateFromDevice &= ~( 0x10 << number );
+void UNIT_4RELAY::LEDWrite(uint8_t number, bool state) {
+    uint8_t StateFromDevice = read1Byte(DEVICE_I2C_ADDR, relay_Reg);
+    if (state == 0) {
+        StateFromDevice &= ~(0x10 << number);
+    } else {
+        StateFromDevice |= (0x10 << number);
     }
-    else
-    {
-        StateFromDevice |= ( 0x10 << number );
-    }
-    write1Byte(addr,0x11,StateFromDevice);
+    write1Byte(DEVICE_I2C_ADDR, 0x11, StateFromDevice);
 }
 
-void UNIT_4RELAY::switchMode( bool mode )
-{
-    write1Byte(addr, 0x10 ,mode);
-}
-
-void UNIT_4RELAY::Init(bool mode)
-{
-    write1Byte(addr,mode_Reg,mode);
-    write1Byte(addr,relay_Reg,0);
+void UNIT_4RELAY::switchMode(bool mode) {
+    write1Byte(DEVICE_I2C_ADDR, 0x10, mode);
 }
--- src/UNIT_4RELAY.h
+++ src/UNIT_4RELAY.h
@@ -1,27 +1,35 @@
 #ifndef _UNIT_4RELAY_H_
 #define _UNIT_4RELAY_H_
-        
+
 #include <Wire.h>
 
-#define addr 0X26
-#define mode_Reg 0X10
-#define relay_Reg 0X11
+#include "pins_arduino.h"
 
+#define DEVICE_I2C_ADDR 0X26
+#define mode_Reg        0X10
+#define relay_Reg       0X11
 
-class UNIT_4RELAY
-{
-    private:
-        void write1Byte(uint8_t address, uint8_t register_address, uint8_t data);
-        uint8_t  read1Byte(uint8_t address, uint8_t register_address);
-    public: 
-        void relayWrite(uint8_t number, bool state);
-        void relayALL(bool state);
-         
-        void LEDWrite(uint8_t number, bool state); 
-        void LED_ALL(bool state);
+class UNIT_4RELAY {
+   private:
+    TwoWire *_wire;
+    uint8_t _sda;
+    uint8_t _scl;
+    uint32_t _freq;
+    void write1Byte(uint8_t address, uint8_t register_address, uint8_t data);
+    uint8_t read1Byte(uint8_t address, uint8_t register_address);
 
-        void switchMode(bool mode);
-        void Init(bool mode);
+   public:
+    void begin(TwoWire *wire = &Wire, uint8_t sda = SDA, uint8_t scl = SCL,
+               uint32_t freq = 4000000UL);
+    void Init(bool mode);
 
+    void relayWrite(uint8_t number, bool state);
+    void relayALL(bool state);
+
+    void LEDWrite(uint8_t number, bool state);
+    void LED_ALL(bool state);
+
+    void switchMode(bool mode);
 };
+
 #endif
